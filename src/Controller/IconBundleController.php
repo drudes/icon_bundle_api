894611ac6be790087498abaec82b68a80238057b
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2022-06-21 01:03:37 +0200

    2022-06-21 01:03

--- src/Controller/IconBundleController.php
+++ src/Controller/IconBundleController.php
@@ -1,13 +1,5 @@
 <?php declare(strict_types=1);
 
-/*
- * This file is part of ptomulik/icon_bundle_api.
- *
- * Copyright (c) Paweł Tomulik <ptomulik@meil.pw.edu.pl>
- *
- * View the LICENSE file for full copyright and license information.
- */
-
 namespace Drupal\icon_bundle_api\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
--- tests/src/Functional/IconBundleApiTest.php
+++ tests/src/Functional/IconBundleApiTest.php
@@ -1,13 +1,5 @@
 <?php declare(strict_types=1);
 
-/*
- * This file is part of ptomulik/icon_bundle_api.
- *
- * Copyright (c) Paweł Tomulik <ptomulik@meil.pw.edu.pl>
- *
- * View the LICENSE file for full copyright and license information.
- */
-
 namespace Drupal\Tests\icon_bundle_api\Functional;
 
 use Drupal\Tests\BrowserTestBase;
